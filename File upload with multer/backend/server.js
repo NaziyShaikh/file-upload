80284e3fa67b3e53c19b81d47ba8dff24e5f3915
naziyaShaikh <naziyahm1234567@gmail.com>
2025-04-08 00:10:04 +0530

    modified server.js

--- File upload with multer/backend/server.js
+++ File upload with multer/backend/server.js
@@ -11,9 +11,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://naziya:wvUfuGUHt26GYPd@cluster0.oswlq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+const mongoURI = 'mongodb+srv://naziya:wvUfuGUHt26GYPd@cluster0.oswlq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
 // Configure storage
 const storage = multer.diskStorage({
